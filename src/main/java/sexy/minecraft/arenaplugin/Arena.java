041473231168996f2305878a9fae201351fefd1b
Salim <niktonick@gmail.com>
2019-02-01 14:17:43 +0100

    PositionPlayers method implemented added temporary method for hard coding starting points added a couple of comments

--- src/main/java/sexy/minecraft/arenaplugin/Arena.java
+++ src/main/java/sexy/minecraft/arenaplugin/Arena.java
@@ -1,15 +1,21 @@
 package sexy.minecraft.arenaplugin;
 
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Arena {
 
+    // TODO load those values from configuration file instead
     public static final int MAX_PLAYERS = 10;
     public static final int MIN_PLAYERS = 2;
     public static final int DELAY = 60;        // delay before the game actually starts
@@ -18,6 +24,9 @@ public class Arena {
     private Set<Player> players = new HashSet<>(MIN_PLAYERS);
     private final ArenaPlugin plugin;
 
+    // points for players to be placed when the game starts. Currently hard coded
+    // TODO load from file instead
+    private Set<Location<World>> startingPoints = hardCodedStartingPoints();
     private StartCountDown startCountDown = new StartCountDown();
 
     public Arena(ArenaPlugin plugin) {
@@ -39,28 +48,37 @@ public class Arena {
     public void addPlayer(Player player) {
 
         players.add(player);
-        if (players.size() == MAX_PLAYERS) {    // if max number of players, start immediately
-            startGame();
+
+        // if max number of players, start immediately
+        if (players.size() == MAX_PLAYERS) {
+
             startCountDown.cancel();
+            startGame();
         }
     }
 
     public void removePlayer(Player player) {
 
         players.remove(player);
-        if (players.isEmpty())  // if last player leaves, remove the arena
+        // if last player leaves, remove the arena
+        if (players.isEmpty())
             die();
     }
 
-    void die() {   //  goes to garbage collector
+    //  go to garbage collector
+    void die() {
+        // stop countdown
         startCountDown.cancel();
+        // set arena field in plugin to null
         plugin.removeArena();
     }
 
-    public void extend(int seconds) {   //  NB: parameter can be negative. Bug or feature?
+    //  NB: parameter can be negative. Bug or feature?
+    public void extend(int seconds) {
         startCountDown.extend(seconds);
     }
 
+    // convenience method for sending simple unformatted text to all players
     private void sendToAllPlayers(String text) {
         players.forEach(p -> p.sendMessage(Text.of(text)));
     }
@@ -68,9 +86,24 @@ public class Arena {
     private void startGame() {
 
         gameStarted = true;
-        sendToAllPlayers("now fight!");
 
-        // here things start happening
+        // TODO save inventory for all players
+        // TODO restrict fast travel commands usage e.g. /spawn, /home, /warp
+        // put players at starting points
+        positionPlayers();
+        sendToAllPlayers("now fight!");
+        // TODO start game (item spawning, restoring losers inventory, determining the winner)
+        die();
+    }
+
+    private void positionPlayers() {
+
+        // linked list implements Deque interface, thus can be used as a stack
+        LinkedList<Location<World>> locations = new LinkedList<>(startingPoints);
+        // shuffle locations so that they appear in random order
+        Collections.shuffle(locations);
+        // teleport every player to location popped from the stack
+        players.forEach(p -> p.setLocation(locations.pop()));
     }
 
     private class StartCountDown {
@@ -115,4 +148,17 @@ public class Arena {
         }
     }
 
+    // TODO remove when not needed
+    private Set<Location<World>> hardCodedStartingPoints() {
+
+        World world = Sponge.getServer().getWorld("world").get();
+        Set<Location<World>> hardcoded = new HashSet<>();
+
+        hardcoded.add(new Location<>(world, 1, 1, 1));
+        hardcoded.add(new Location<>(world, 500, 200, 500));
+        hardcoded.add(new Location<>(world, 255, 100, 255));
+        // ...
+
+        return hardcoded;
+    }
 }
