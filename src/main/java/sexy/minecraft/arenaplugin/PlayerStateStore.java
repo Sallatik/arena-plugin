d7500241552f02c45d89628e5d515e82bdd15ceb
Salim <niktonick@gmail.com>
2019-03-10 00:28:53 +0100

    fixed winner

--- src/main/java/sexy/minecraft/arenaplugin/PlayerStateStore.java
+++ src/main/java/sexy/minecraft/arenaplugin/PlayerStateStore.java
@@ -1,8 +1,10 @@
 package sexy.minecraft.arenaplugin;
 
+import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.entity.living.humanoid.player.RespawnPlayerEvent;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
@@ -17,8 +19,6 @@ import static sexy.minecraft.arenaplugin.Util.*;
 
 public class PlayerStateStore {
 
-    private static final int NUM_OF_SLOTS = 41;
-
     private Map<Player, InventorySnapshot> savedInventories = new HashMap<>();
     private Set<Player> toRestore = new HashSet<>();
 
@@ -27,8 +27,11 @@ public class PlayerStateStore {
     }
 
     public void restore(Player player) {
-        if(savedInventories.containsKey(player))
+
+        if(savedInventories.containsKey(player)) {
             toRestore.add(player);
+            tryRestore(player);
+        }
     }
 
     @Listener
@@ -36,9 +39,23 @@ public class PlayerStateStore {
 
         Player player = event.getTargetEntity();
         if(toRestore.contains(player))
-            restoreInventory(player);
+            tryRestore(player);
     }
 
+    @Listener
+    public void onClientJoin(ClientConnectionEvent.Join event) {
+
+        Player player = event.getTargetEntity();
+        if (toRestore.contains(player))
+            tryRestore(player);
+    }
+
+    private void tryRestore(Player player) {
+
+        if(player.get(Keys.HEALTH).get() > 0 && player.isOnline())
+            restoreInventory(player);
+
+    }
     private void saveInventory(Player player) {
         savedInventories.put(player, new InventorySnapshot(player.getInventory()));
     }
